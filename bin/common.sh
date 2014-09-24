57628dbf4a34b1561d6c7b32119f68e8ac4b2af5
Marcin Sikon <marcin.sikon@schibsted.pl>
2014-09-24 21:07:20 +0200

    Make sure that php-fpm can write to the log fifos (user = nobody)

--- bin/common.sh
+++ bin/common.sh
@@ -40,6 +40,7 @@ init_log_plex_fifo() {
   for log_file in $*; do
     echo "rm -f ${log_file}"
     echo "mkfifo ${log_file}"
+    echo "chown nobody: ${log_file}"
   done
 }
 
@@ -49,6 +50,7 @@ init_log_plex() {
   done
   for log_file in $*; do
     echo "touch ${log_file}"
+    echo "chown nobody: ${log_file}"
   done
 }
 
